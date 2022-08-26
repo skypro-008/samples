d8ba3b437229cfbc965e44b62df666b60cc28d63
Gleb Kushedow <kushedow@gmail.com>
2022-08-26 10:32:53 +0300

    lesson 6 / read_from_nested

---
+++ course_2/lesson_6/read_from_nested/inner_folder/main.py
@@ -0,0 +1,5 @@
+
+
+with open("data.txt") as file:
+    contents = file.read()
+    print(contents)
---
+++ course_2/lesson_6/read_from_nested/main.py
@@ -0,0 +1,5 @@
+from inner_folder import main
+
+with open("data.txt") as file:
+    contents = file.read()
+    print(contents)
