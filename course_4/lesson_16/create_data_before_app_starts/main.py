7ed4e04b15a2412c531bd528503c655145311b25
Gleb Kushedow <kushedow@gmail.com>
2022-10-25 22:12:54 +0400

    lesson 12 / fix blueprints, lesson 10 / debug

---
+++ course_4/lesson_16/create_data_before_app_starts/main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class UserModel(db.Model):
+    """Модель пользователя"""
+    __tablename__ = 'user'
+
+    pk = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String, nullable=True)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    # Пересоздаем базу данных
+
+    #
+    # # Создаем пользователей
+    user_1 = UserModel(pk=1, first_name="Алиса")
+    user_2 = UserModel(pk=2, first_name="Денис")
+    #
+    # # Загружаем все в базу данных
+    db.session.add_all([user_1, user_2])
+    db.session.commit()
